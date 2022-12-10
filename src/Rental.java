06920c69d16aa85c53876d1ba4332e3799e7454b
Kamil Falgowski <szlyrda@gmail.com>
2022-12-10 18:55:06 +0100

    client update

--- src/Rental.java
+++ src/Rental.java
@@ -1,4 +1,25 @@
-public class Rental {
+public class Rental extends Client{
+
+    int startDate;
+    int endDate;
+    double toPay;
+
+    public void rentVehicle(){
+
+    }
+
+    public void returnPayment(){
+
+    }
+
+    public void addRentalTime(){
+
+    }
+
+    public void findRental(){
+
+    }
+
 
 
 }
